04f6c5aef28b940307ab7df155834a5e5653b3eb
captjay98 <captjay98@gmail.com>
2024-02-21 20:56:57 +0100

    diable admin

--- medusa-config.js
+++ medusa-config.js
@@ -48,6 +48,7 @@ const plugins = [
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: false,
+      serve: process.env.NODE_ENV === "development",
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
