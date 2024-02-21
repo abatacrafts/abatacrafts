a66e3a9e8042e92a5c0b9f2c5cb6f10a1ed717e6
captjay98 <captjay98@gmail.com>
2024-02-21 19:54:43 +0100

    trying to build with admin again

--- medusa-config.js
+++ medusa-config.js
@@ -47,7 +47,7 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
-      autoRebuild: false,
+      autoRebuild: true,
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
