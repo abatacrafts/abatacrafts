eaee918a301677e419e8027b08938043ba37fd99
captjay98 <captjay98@gmail.com>
2024-02-21 23:41:22 +0100

    Removing admin from backend

--- medusa-config.js
+++ medusa-config.js
@@ -48,7 +48,7 @@ const plugins = [
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: false,
-      serve: process.env.NODE_ENV === "development",
+      serve: false,
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
